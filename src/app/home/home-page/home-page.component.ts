68b5922106b970970e24fb7ea9f2a15bf20da2f1
Ingrid Carvalho <ingridcarvalho_1007@hotmail.com>
2019-10-22 22:32:59 -0300

    Implemented notifications

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,7 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { InvoiceService } from '../../invoices/invoice/invoice.service';
+import { UserService } from '../../core/user/user.service';
 
 @Component({
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent{}
+export class HomePageComponent implements OnInit {
+
+    pending: number = 0;
+    disapprove: number = 0;
+    approve: number = 0;
+    onRequest: number = 0;
+
+    constructor(
+        private invoiceService: InvoiceService,
+        private userService: UserService
+    ){}
+
+    ngOnInit(): void {
+        this.countStatus();
+    }
+
+    countStatus(){
+        this.invoiceService.listInvoices()
+            .subscribe(invoices => {
+                invoices.forEach(invoice => {
+                    switch (invoice.status) {
+                        case 'Pendente':
+                            this.pending++
+                            break;
+                        case 'Solicitado Antecipação':
+                            this.onRequest++
+                            break;
+                        case 'Aprovado':
+                            this.approve++
+                            break;
+                        case 'Reprovado':
+                            this.disapprove++
+                            break;
+                    }
+                })
+            })
+    }
+}
--- src/app/shared/components/panel-notifications/panel-notifications.component.ts
+++ src/app/shared/components/panel-notifications/panel-notifications.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-panel',
-    templateUrl: 'panel-notifications.component.html'
+    templateUrl: 'panel-notifications.component.html',
+    styleUrls: ['./panel-notifications.component.css']
 })
 export class PanelNotifications{
     
+    @Input() title: string = '';
+    @Input() labelOne: string = '';
+    @Input() labelTwo: string = '';
+    @Input() labelThree: string = '';
+    @Input() labelFour: string = '';
+    @Input() valueOne: string = '';
+    @Input() valueTwo: string = '';
+    @Input() valueThree: string = '';
+    @Input() valueFour: string = '';
 }
